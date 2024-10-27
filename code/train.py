02641e96c0f18d850fcab1400dc708c8dd60992d
Sabina Talipova <87288324+sabina-talipova@users.noreply.github.com>
2024-10-27 22:41:07 +1300

    Add training method with cross validation on full dataset

--- code/train.py
+++ code/train.py
@@ -9,13 +9,16 @@ import shutil
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 from torchvision import datasets, transforms, models
 from dotenv import load_dotenv
+from tqdm import tqdm
+
+from sklearn.model_selection import KFold
 
 DATA_FOLDER_PATH = "train_data"
 # DATA_FOLDER_PATH = os.environ['FILE_PATH']
-MODEL_FILE_PATH = 'model_2.pth'
+MODEL_FILE_PATH = 'model.pth'
 
 INPUT_SIZE = 224   # Image 224x224 and 3 channels
 HIDDEN_SIZE = 128  # Features in hidden layer
@@ -249,6 +252,95 @@ class ModelTrainer:
 
         print("Model training DONE")
 
+    def train_model_CV(self, num_epochs=10):
+        print("Start Cross-validation model training...")
+
+        BATCH_SIZE = 32
+        NUM_EPOCHS = 5
+        NUM_FOLDS = 5
+
+        train_transform = transforms.Compose([
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomRotation(20),
+            transforms.RandomResizedCrop(INPUT_SIZE),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+            transforms.Resize((INPUT_SIZE, INPUT_SIZE)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+        def is_valid_file(file_path):
+            valid_extensions = ('.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm', '.tif', '.tiff', '.webp')
+            if "ipynb" in file_path:
+                return False
+            return True
+
+        dataset = datasets.ImageFolder(root=self.dir, transform=train_transform, is_valid_file=is_valid_file)
+
+        kf = KFold(n_splits=NUM_FOLDS, shuffle=True, random_state=42)
+
+
+        criterion = nn.CrossEntropyLoss()
+
+        for fold, (train_idx, val_idx) in enumerate(kf.split(dataset)):
+            print(f'Fold {fold + 1}/{NUM_FOLDS}')
+
+            train_subset = Subset(dataset, train_idx)
+            val_subset = Subset(dataset, val_idx)
+
+            train_loader = DataLoader(train_subset, batch_size=BATCH_SIZE, shuffle=True)
+            val_loader = DataLoader(val_subset, batch_size=BATCH_SIZE, shuffle=False)
+
+            model = self.__model
+            optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            model = model.to(device)
+
+            for epoch in range(NUM_EPOCHS):
+
+                model.train()
+                running_loss = 0.0
+                for inputs, labels in tqdm(train_loader, desc=f"Training Fold {fold + 1}, Epoch {epoch + 1}"):
+                    inputs, labels = inputs.to(device), labels.to(device)
+                    optimizer.zero_grad()
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels)
+                    loss.backward()
+                    optimizer.step()
+                    running_loss += loss.item()
+
+                print(f"Fold {fold + 1}, Epoch {epoch + 1}, Training Loss: {running_loss / len(train_loader)}")
+
+                model.eval()
+                val_loss = 0.0
+                correct = 0
+                total = 0
+                with torch.no_grad():
+                    for inputs, labels in val_loader:
+                        inputs, labels = inputs.to(device), labels.to(device)
+                        outputs = model(inputs)
+                        loss = criterion(outputs, labels)
+                        val_loss += loss.item()
+
+                        _, predicted = torch.max(outputs, 1)
+                        total += labels.size(0)
+                        correct += (predicted == labels).sum().item()
+
+                print(f"Fold {fold + 1}, Epoch {epoch + 1}, Validation Loss: {val_loss / len(val_loader)}, "
+                      f"Accuracy: {100 * correct / total}%")
+
+        print("Model training DONE")
+
+        try:
+            torch.save({
+                'model_state_dict': self.__model.state_dict(),
+                'optimizer_state_dict': self.__optimizer.state_dict(),
+            }, MODEL_FILE_PATH)
+            print("Model was saved in model.pth")
+        except Exception as e:
+            print(f"Error saving the model: {e}")
+
 
 def eda():
     load_dotenv()
@@ -271,7 +363,8 @@ def main():
     load_dotenv()
     print("Training model...")
     my_object = ModelTrainer(DATA_FOLDER_PATH, CustomCNN())
-    my_object.create_model()
+    my_object.train_model_CV()
+    # my_object.create_model()
     print("Training model DONE")
 
 if __name__ == "__main__":
